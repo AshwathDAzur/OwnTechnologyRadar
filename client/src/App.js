fe1c242e32c66f78534f0a7f71fea2bd9173678f
Ashwath kumaran <a4ashwath@gmail.com>
2023-09-10 21:12:46 +0530

    Client Update 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import Demo from "./Components/demo";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Landing from "./Pages/Landing";
+import Page1 from "./Pages/Page1";
+import Page2 from "./Pages/Page2";
+import Page3 from "./Pages/Page3";
 
 function App() {
   return (
     <div className="App">
-      <Demo />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/page1" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+          <Route path="/page3" element={<Page3 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/Pages/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Appbar from "../Components/Appbar";
+
+export default function Landing() {
+  return (
+    <div>
+      <Appbar />
+      <h1>Landing Page...</h1>
+    </div>
+  );
+}
